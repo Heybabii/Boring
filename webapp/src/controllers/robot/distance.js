abe265b73baead12802998335bab381f99492de3
Heybabii <best_n.dol@hotmail.com>
2019-11-30 18:30:13 +0700

    Wait for database

--- webapp/src/controllers/robot/distance.js
+++ webapp/src/controllers/robot/distance.js
@@ -1,21 +1,47 @@
 export default (req, res, next) => {
+    
     const { first_pos, second_pos , metric } = req.body;
-    if (!first_pos || !second_pos) return res.status(424).sand("Insufficient data to compute the result");
-    const { x:fx,y:fy } = first_pos;
-    const { x:sx,y:sy } = second_pos;
-    if (!fx || !fy || !sx || !sy ) return res.status(400).send("Request was ill-formed");
-    if(isNaN(fx) || isNaN(fy) || isNaN(sx) || isNaN(sy)) return res.status(400).send("Request was ill-formed");
+    var x1,y1,x2,y2;
+
+    if (!first_pos || !second_pos) return res.status(424).send("Insufficient data to compute the result");
+    if(typeof first_pos == "string"){
+        if(/^robotF([1-9][0-9]*)$/.test(first_pos)){
+            const id = first_pos.match(/[1-9][0-9]/);
+            x1 = FROM_ID_X1;//for por
+            y1 = FROM_ID_Y1;//for por
+        }
+        else return res.status(400).send("Request was ill-formed");
+    }
+    else{
+        const { x:fx,y:fy } = first_pos;
+        if (!fx || !fy) return res.status(400).send("Request was ill-formed");
+        const p1 = first_pos;
+        x1 = p1.x;
+        y1 = p1.y;
+    }
+
+    if(typeof second_pos == 'string'){
+        if(/^robotF([1-9][0-9]*)$/.test(second_pos)){
+            const id = first_pos.match(/[1-9][0-9]/);
+            x2 = FROM_ID_X2;//for por
+            y2 = FROM_ID_Y2;//for por
+        }
+        else return res.status(400).send("Request was ill-formed");
+    }
+    else{
+        const { x:sx,y:sy } = second_pos;
+        if (!sx || !sy ) return res.status(400).send("Request was ill-formed");
+        const p2 = second_pos;
+        x2 = p2.x;
+        y2 = p2.y;
+    }
     if (metric) {
         if(metric == "euclidean"){
-            const p1 = first_pos;
-            const p2 = second_pos;
-            const distance = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+            const distance = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
             return res.status(200).json( { distance });
         }
         else if(metric == "manhattan"){
-            const p1 = first_pos;
-            const p2 = second_pos;
-            const distance = Math.abs(p1.x-p2.x)+Math.abs(p1.y-p2.y);
+            const distance = Math.abs(x1-x2)+Math.abs(y1-y2);
             return res.status(200).json( { distance });
         }
         else{
@@ -23,9 +49,7 @@ export default (req, res, next) => {
         }
     }
     else {
-        const p1 = first_pos;
-        const p2 = second_pos;
-        const distance = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y));
+        const distance = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
         return res.status(200).json( { distance });
     }
 }
