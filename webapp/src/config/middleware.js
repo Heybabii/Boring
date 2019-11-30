d31014b0b30b553cb6e88c2234fb45177bc86ae6
dragonfly161518 <por.nattapat.01@gmail.com>
2019-11-30 15:55:02 +0700

    Remove old dependencies

--- webapp/src/config/middleware.js
+++ webapp/src/config/middleware.js
@@ -1,13 +1,8 @@
 import bodyParser from 'body-parser';
-// import passport from '../passport';
 import cors from 'cors';
-
-// const isTest = process.env.NODE_ENV === 'test';
-// const isDev = process.env.NODE_ENV === 'development';
 
 export default app => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  // app.use(passport.init());
   app.use(cors());
 };
--- webapp/src/controllers/auth/index.js
+++ webapp/src/controllers/auth/index.js
@@ -1,20 +1,16 @@
-import { Router as router } from 'express';
-// import passport from 'passport';
-import signIn from './sign-in';
-import signUp from './sign-up';
+// // import { Router as router } from 'express';
+// // import passport from 'passport';
+// import signIn from './sign-in';
+// import signUp from './sign-up';
 
-export { signIn, signUp };
+// export { signIn, signUp };
 
-export default (models, { config }) => {
-  const api = router();
+// export default (models, { config }) => {
+//   const api = router();
 
-  api.post(
-    '/signin',
-    passport.authenticate('local', { session: false }),
-    signIn,
-  );
+//   api.post('/signin', signIn);
 
-  api.post('/signup', signUp(models));
+//   api.post('/signup', signUp(models));
 
-  return api;
-};
+//   return api;
+// };
